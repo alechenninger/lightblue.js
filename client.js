2a3715a95901e7c31ca14af1a1ff5e5ae662f743
Alec Henninger <ahenning@redhat.com>
2014-10-28 22:06:04 -0400

    Add insert request

--- client.js
+++ client.js
@@ -6,6 +6,10 @@ LightblueClient.prototype.find = function(data) {
   return new FindRequest(this, data);
 };
 
+LightblueClient.prototype.insert = function(data) {
+  return new InsertRequest(this, data);
+};
+
 function LightblueRequest(client, method, data, url) {
   this.client = client;
   this.method = (typeof method === "string") ? method.toLowerCase() : "";
@@ -14,19 +18,53 @@ function LightblueRequest(client, method, data, url) {
   this.url = url;
 }
 
+/**
+ * @param data The JSON data of request
+ */
 function FindRequest(client, data) {
   var url = "";
 
   if (client instanceof Object && data instanceof Object) {
-    url = client.host + "/data/find/" + data.entity;
-
-    if (typeof data.version === "string") {
-      url += "/" + data.version;
-    }
+    url = resolve(client.host, "data/insert", data.entity, data.version);
   }
 
-  LightblueRequest.call(this, client, "get", data, url);
+  LightblueRequest.call(this, client, "post", data, url);
 }
 
 FindRequest.prototype = new LightblueRequest();
 FindRequest.prototype.constructor = FindRequest;
+
+/**
+ * @param data The JSON data of request
+ */
+function InsertRequest(client, data) {
+  var url = "";
+
+  if (client instanceof Object && data instanceof Object) {
+    url = resolve(client.host, "data/insert", data.entity, data.version);
+  }
+
+  LightblueRequest.call(this, client, "put", data, url);
+}
+
+InsertRequest.prototype = new LightblueRequest();
+InsertRequest.prototype.constructor = InsertRequest;
+
+function resolve() {
+  if (arguments.length === 0) {
+    return "";
+  }
+  
+  function trimSlashes(s) {
+    if (typeof s !== "string") {
+      return "";
+    }
+    
+    s = s.replace(new RegExp("^/*"), "");
+    s = s.replace(new RegExp("/*$"), "");
+    
+    return s;
+  }
+  
+  return Array.prototype.slice.call(arguments, 0).map(trimSlashes).join("/");
+}
