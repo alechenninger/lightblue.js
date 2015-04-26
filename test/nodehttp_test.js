4c47070a97e931e3464928209f20d4ffc5b7e6ce
Alec Henninger <alechenninger@gmail.com>
2015-04-26 03:40:24 +0000

    Add some basic tests for node compatible http client

---
+++ test/nodehttp_test.js
@@ -0,0 +1,58 @@
+/* globals describe it before */
+var NodeHttpClient = require("../lib/nodehttp.js").NodeHttpClient;
+var HttpRequest    = require("../lib/http.js").HttpRequest;
+var nock           = require("nock");
+var expect         = require("chai").expect;
+
+describe("NodeHttpClient", function() {
+  var client;
+  
+  before(function() {
+    client = new NodeHttpClient();
+  });
+  
+  it("makes a get request against url with path", function(done) {
+    nock("http://foo.com")
+        .get("/api")
+        .reply(200);
+    
+     client.execute(new HttpRequest("get", "http://foo.com/api"))
+        .then(function(response) {
+          done();
+        });
+  });
+  
+  it("makes a get requst against url without path", function(done) {
+    nock("http://foo.com")
+        .get("/")
+        .reply(200);
+        
+    client.execute(new HttpRequest("get", "http://foo.com"))
+        .then(function(response) {
+          done();
+        });
+  });
+  
+  it("makes a post request with json body", function(done) {
+    nock("http://foo.com")
+        .post("/", {foo: "bar"})
+        .reply(200);
+      
+    client.execute(new HttpRequest("post", "http://foo.com", {foo: "bar"}))
+        .then(function(response) {
+          done();
+        });
+  });
+  
+  it("makes request and completes promise with success response body", function(done) {
+    nock("http://foo.com")
+      .get("/")
+      .reply(200, "it works");
+      
+    client.execute(new HttpRequest("get", "http://foo.com"))
+      .then(function(response) {
+        expect(response).to.equal("it works");
+        done();
+      });
+  });
+});
