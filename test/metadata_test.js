1e0b22f5cfd49a07f8a92f120bf5fbcc99e4fb9a
Alec Henninger <ahenning@redhat.com>
2015-07-11 20:01:46 -0400

    Add tests for get metadata for entity version API

--- test/metadata_test.js
+++ test/metadata_test.js
@@ -3,7 +3,7 @@ var expect = require("chai").expect;
 var MetadataClient = require("../lib/metadata.js");
 
 describe("LightblueMetadataClient", function() {
-  
+
   // Captures request sent to execute(req)
   var mockHttpClient = {
     execute: function(request) {
@@ -11,9 +11,9 @@ describe("LightblueMetadataClient", function() {
       return "response";
     }
   };
-  
+
   var metadataClient = new MetadataClient(mockHttpClient, "myhost.com");
-  
+
   describe("getNames", function() {
     it("should construct urls like ${host}/ when no status is provided", function() {
       metadataClient.getNames();
@@ -38,140 +38,91 @@ describe("LightblueMetadataClient", function() {
 
       expect(mockHttpClient.request.method).to.equal("get");
     });
-    
+
     it("should return result of httpclient execute", function() {
       var result = metadataClient.getNames();
-      
+
       expect(result).to.equal("response");
     });
   });
-  
+
   describe("getVersions", function() {
     it("should construct urls like ${host}/${entityName}", function() {
       metadataClient.getVersions("foo");
-      
+
       expect(mockHttpClient.request.url).to.match(new RegExp("^myhost.com/foo/?$"));
     });
-    
+
     it("should use GET", function() {
       metadataClient.getVersions("foo");
-      
+
       expect(mockHttpClient.request.method).to.equal("get");
     });
-    
+
     it("should return response from httpclient execute", function() {
       var result = metadataClient.getVersions("foo");
-      
+
       expect(result).to.equal("response");
     });
   });
-  
+
   describe("getRoles", function() {
     it("should construct urls like ${host}/roles when no entity or version is provided", function() {
       metadataClient.getRoles();
-      
+
       expect(mockHttpClient.request.url).to.match(new RegExp("^myhost.com/roles/?$"));
     });
-    
+
     it("should construct urls like ${host}/${entityName}/roles when an entity name but no version is provided", function() {
       metadataClient.getRoles("foo");
-      
+
       expect(mockHttpClient.request.url).to.match(new RegExp("^myhost.com/foo/roles/?$"));
     });
-    
+
     it("should construct urls like ${host}/${entityName}/{$version}/roles when entity name and version are provided", function() {
       metadataClient.getRoles("foo", "1.0.0");
-      
+
       expect(mockHttpClient.request.url).to.match(new RegExp("^myhost.com/foo/1.0.0/roles/?$"));
     });
-    
+
     it("should use GET", function() {
       metadataClient.getRoles();
-      
+
       expect(mockHttpClient.request.method).to.equal("get");
     });
-    
+
     it("should return result of httpclient execute", function() {
       var result = metadataClient.getRoles();
-      
+
+      expect(result).to.equal("response");
+    });
+  });
+
+  describe("getMetadata", function() {
+    it("should construct urls like ${host}/${entityName}/${version}", function() {
+      metadataClient.getMetadata("foo", "1.2.3");
+
+      expect(mockHttpClient.request.url).to.match(new RegExp("^myhost.com/foo/1.2.3"));
+    });
+
+    it("should require name and version to be specified", function() {
+      expect(metadataClient.getMetadata).to.throw(Error);
+      expect(function() { metadataClient.getMetadata("foo"); }).to.throw(Error);
+      expect(function() { metadataClient.getMetadata("foo", ""); }).to.throw(Error);
+      expect(function() { metadataClient.getMetadata("", "1"); }).to.throw(Error);
+    });
+
+    it("should use GET", function() {
+      metadataClient.getMetadata("foo", "1");
+      expect(mockHttpClient.request.method).to.equal("get");
+    });
+
+    it("should return result of httpclient execute", function() {
+      var result = metadataClient.getMetadata("foo", "1");
       expect(result).to.equal("response");
     });
   });
 });
-
-function validFindConfig(edit) {
-  edit = edit || {};
-
-  var config = {
-    entity: ifDefined(edit.entity, "user"),
-    version: ifDefined(edit.version, "1.0"),
-    query: ifDefined(edit.query, { field: "name", op: "$eq", "rvalue": "Bob" }),
-    projection: ifDefined(edit.projection, { include: "*" })
-  };
-
-  if (edit.sort) {
-    config.sort = edit.sort;
-  }
-
-  if (edit.range) {
-    config.range = edit.range;
-  }
-
-  return config;
-}
-
-function validInsertConfig(edit) {
-  edit = edit || {};
-
-  return {
-    entity: ifDefined(edit.entity, "user"),
-    version: ifDefined(edit.version, "1.0"),
-    data: ifDefined(edit.data, [{
-      name: "bob"
-    }]),
-    projection: ifDefined(edit.projection, { include: "*" })
-  };
-}
-
-function validUpdateConfig(edit) {
-  edit = edit || {};
-
-  return {
-    entity: ifDefined(edit.entity, "user"),
-    version: ifDefined(edit.version, "1.0"),
-    query: ifDefined(edit.query, { field: "name", op: "$eq", "rvalue": "Bob" }),
-    update: ifDefined(edit.update, {
-      $set: {
-        name: "Jim"
-      }
-    }),
-    projection: ifDefined(edit.projection, { include: "*" })
-  };
-}
-
-function validSaveConfig(edit) {
-  edit = edit || {};
-
-  return {
-    entity: ifDefined(edit.entity, "user"),
-    version: ifDefined(edit.version, "1.0"),
-    data: ifDefined(edit.data, [{
-      name: "bob"
-    }]),
-    upsert: ifDefined(edit.upsert, true),
-    projection: ifDefined(edit.projection, { include: "*" })
-  };
-}
-
-function validDeleteConfig(edit) {
-  edit = edit || {};
-
-  return {
-    entity: ifDefined(edit.entity, "user"),
-    version: ifDefined(edit.version, "1.0"),
-    query: ifDefined(edit.query, { field: "name", op: "$eq", "rvalue": "Bob" })
-  };
-}
 
 function ifDefined(it, otherwise) {
   return (typeof it !== "undefined") ? it : otherwise;
